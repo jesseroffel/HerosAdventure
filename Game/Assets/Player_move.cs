a4c10353bb3721cb3fdd519548e554d9a52f4869
quintin <quintiniwema@hotmail.com>
2016-09-26 15:52:27 +0200

    no message

---
+++ Game/Assets/Player_move.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Player_move : MonoBehaviour
+{
+    public float speed = 1;
+    bool CanJump;
+    bool isWalking;
+
+    public Animator anim;
+    public Rigidbody RB;
+
+	void Start ()
+    {
+        
+	}
+	
+
+	void FixedUpdate ()
+    {
+	    if(Input.GetKey(KeyCode.W))
+        {
+            isWalking = true;
+           // transform.Translate(Vector3.forward * speed * Time.deltaTime);
+            anim.SetFloat("speed", 1.1f);
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            isWalking = true;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            isWalking = true;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            isWalking = true;
+        }
+
+
+        if(RB.velocity.magnitude == 0.0)
+        {
+            anim.SetFloat("speed", 0.0f);
+        }
+    }
+}
