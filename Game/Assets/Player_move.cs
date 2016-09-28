b3f1556c6a79615335d887afe0b5a5cbeb8043cf
quintin <quintiniwema@hotmail.com>
2016-09-28 16:06:47 +0200

    animation code added to player

--- Game/Assets/Player_move.cs
+++ Game/Assets/Player_move.cs
@@ -3,8 +3,6 @@ using System.Collections;
 
 public class Player_move : MonoBehaviour
 {
-  //  public float rotationspeed;
-   // Quaternion targetrotation;
     public Rigidbody rb;
     float forwardInput, sideInput;
 
@@ -12,11 +10,8 @@ public class Player_move : MonoBehaviour
     public float speed = 1;
     float angle;
 
-  //  float dir;
-    //float horizontal;
-   // float vertical;
-
-    
+    public Animator anim;
+  
 	void Start ()
     {
      //   targetrotation = transform.rotation;
@@ -44,9 +39,7 @@ public class Player_move : MonoBehaviour
     void Move()
     {
         transform.Translate(Vector3.forward * forwardInput * speed * Time.deltaTime);
-       // transform.Translate(Vector3.right * sideInput * speed * Time.deltaTime);
 
-        Debug.Log(sideInput);
         if (sideInput < 0)
         {
             angle = -1;
@@ -57,34 +50,11 @@ public class Player_move : MonoBehaviour
             angle = 1;
             transform.RotateAround(cam.transform.position, new Vector3(0, 1, 0), angle);
         }
-
-       
+        anim.SetFloat("speed", 2);
     }
 
-    /*void Orbit(Transform root, Transform cam, float dirOut, float speedOut)
+    void Jump()
     {
-        Vector3 rootDir = root.forward;
 
-        Vector3 moveKeyDir = new Vector3(horizontal, 0, vertical);
-
-        speedOut = moveKeyDir.sqrMagnitude;
-
-        //get camera rotation
-        Vector3 cameraDirection = cam.forward;
-        cameraDirection.y = 0.0f; // kill y
-        Quaternion referentislShift = Quaternion.FromToRotation(Vector3.forward, cameraDirection);
-
-        //convert movekeys to worldspace cords
-        Vector3 moveDir = referentislShift * moveKeyDir;
-        Vector3 axisSign = Vector3.Cross(moveDir, rootDir);
-
-        float angleRootToMove = Vector3.Angle(rootDir, moveDir) * (axisSign.y >= 0 ? -1f : 1f);
-
-        angleRootToMove /= 180f;
-
-        dirOut = angleRootToMove * speed;
-        
     }
-
-    */
 }
