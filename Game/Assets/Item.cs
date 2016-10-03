fe61085fb6f18312b2abeb3081276e0806fabac9
quintin iwema <quintiniwema@hotmail.com>
2016-10-03 19:46:30 +0200

    item update

--- Game/Assets/Item.cs
+++ Game/Assets/Item.cs
@@ -1,17 +1,35 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Item : MonoBehaviour
+[System.Serializable]
+public class Item
 {
+    public int itemID;
+    public string itemName;
+    public string itemDisc;
+    public Texture2D itemIcon;
+    public int itemPower;
+    public int itemSpeed;
+    public ItemType itemType;
 
+    public enum ItemType
+    {
+        weapon,
+        Consumable,
+        Quest,
+        thrash
+    }
 
-	void Start ()
+    public Item(string name, int id, string disc, int power, int speed, ItemType type)
     {
-	
-	}
-	
-	void Update ()
+        itemID = id;
+        itemName = name;
+        itemDisc = disc;
+        itemPower = power;
+        itemSpeed = speed;
+        itemType = type;
+    }
+    public Item()
     {
-	
-	}
+    }
 }
