31cbf48f16f29dee5da6f562d449d885ba1e55ea
quintin <quintiniwema@hotmail.com>
2016-10-04 14:29:46 +0200

    inventory update

---
+++ Game/Assets/Scripts/Inventory.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class Inventory : MonoBehaviour
+{
+    public int SlotY;
+  //  public GUISkin skin;
+    public List<Item> inventory = new List<Item>();
+
+    private ItemDatabase database;
+    private bool showInventory;
+
+    private bool showToolTip;
+    private string Tooltip;
+
+	void Start ()
+    {
+	
+	}
+	
+
+	void Update ()
+    {
+	    if(Input.GetKeyDown(KeyCode.I))        
+        {
+            Debug.Log("open inventory"); 
+        }
+	}
+
+    void OnGui()
+    {
+        Tooltip = "";
+       // GUI.skin = skin;
+       if(showInventory)
+        {
+            DrawInventory();
+        }
+    }
+
+    void DrawInventory()
+    {
+        int i;
+        for(i = 0; i < inventory.Count; i++)
+        {
+            
+        }
+    }
+
+    void UseConsumable()
+    {
+
+    }
+
+    void RemoveItem()
+    {
+
+    }
+
+    void AddItem()
+    {
+
+    }
+
+    bool InventoryContains()
+    {
+        bool result = false;
+
+        return result;
+    }
+}
---
+++ Game/Assets/Scripts/ItemDatabase.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ItemDatabase : MonoBehaviour
+{
+    public List<Item> items = new List<Item>();
+
+    void Start()
+    {
+        items.Add(new Item("training Sword", 0, "a basic trainging sword", 1, 1, Item.ItemType.weapon));
+        items.Add(new Item("potion", 1, "a basic health potion", 0, 0, Item.ItemType.Consumable));
+    }
+
+}
