685626e591a3b445139ec0b1d95435a3b7855875
quintin <quintiniwema@hotmail.com>
2016-10-10 16:03:50 +0200

    update player controler

--- Game/Assets/Scripts/testcamera.cs
+++ Game/Assets/Scripts/testcamera.cs
@@ -11,26 +11,18 @@ public class testcamera : MonoBehaviour {
     public float distanceY;
     public float distanceZ;
 
-    public float followDist = 5;
-    float dist;
-
     void Start ()
-    {
-       dist = Vector3.Distance(player.transform.position, transform.position);
-        Debug.Log(followDist);
+    {       
      //   player = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
-        transform.position = new Vector3(player.transform.position.x - distanceX, player.transform.position.y - distanceY, player.transform.position.z - distanceZ);
-             
+        transform.position = new Vector3(player.transform.position.x - distanceX, player.transform.position.y - distanceY, player.transform.position.z - distanceZ);             
     }
 
     void Update()
-    {
-        if(dist > followDist)
-        {
-            transform.Translate(new Vector3(player.transform.position.x - distanceX, player.transform.position.y - distanceY, player.transform.position.z - distanceZ));
-        }
-
-         transform.LookAt(lookAtTarget);
-         transform.RotateAround(player.position, Vector3.up, Input.GetAxis("Mouse X") * rotateSpeed);
+    {       
+        transform.LookAt(lookAtTarget);
+        //Vector3 playerPos = new Vector3(player.transform.position.x - distanceX, player.transform.position.y - distanceY, player.transform.position.z - distanceZ);
+        transform.RotateAround(player.position, Vector3.up, Input.GetAxis("Mouse X") * rotateSpeed);
+        
+        
     }
 }
