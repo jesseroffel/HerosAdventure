3ed48257ebe087d1549c62bf33aee40e28d58541
quintin iwema <quintiniwema@hotmail.com>
2016-10-10 21:00:05 +0200

    adden inventory system

---
+++ Game/Assets/Inventory.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+
+public class Inventory : MonoBehaviour
+{
+    GameObject inventoryPanel;
+    GameObject slotPanel;
+    ItemDatabase database;
+    public GameObject inventorySlot;
+    public GameObject inventoryItem;
+
+    public int slotAmount = 20;
+
+    public List<Item> items = new List<Item>();
+    public List<GameObject> slots = new List<GameObject>();
+
+
+
+    void Start()
+    {
+        inventoryPanel = GameObject.Find("Inventory Panel");
+        slotPanel = inventoryPanel.transform.FindChild("Slot Panel").gameObject;
+        database = GetComponent<ItemDatabase>();
+
+        for(int i=0; i < slotAmount; i++)
+        {
+            items.Add(new Item());
+            slots.Add(Instantiate(inventorySlot));
+            slots[i].transform.SetParent(slotPanel.transform);
+        }
+
+        AddItem(1);
+        AddItem(0);
+        AddItem(1);
+        AddItem(1);
+    }
+
+    public void AddItem(int id)
+    {
+        Item itemToAdd = database.FetchItemByID(id);
+        for(int i = 0; i< items.Count; i++)
+        {
+            if(items[i].ID == -1)
+            {
+                items[i] = itemToAdd;
+                GameObject itemObj = Instantiate(inventoryItem);
+                itemObj.transform.SetParent(slots[i].transform);               
+                itemObj.transform.position = Vector2.zero;
+                itemObj.GetComponent<Image>().sprite = itemToAdd.Sprite;
+                itemObj.name = itemToAdd.Title;
+
+                break;
+            }
+        }
+    }
+
+}
---
+++ Game/Assets/ItemDatabase.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using LitJson;
+using System.Collections.Generic;
+using System.IO;
+
+public class ItemDatabase : MonoBehaviour
+{
+    private List<Item> database = new List<Item>();
+    private JsonData itemData;
+
+    void Start()
+    {
+        itemData = JsonMapper.ToObject(File.ReadAllText(Application.dataPath + "/StreamingAssets/Items.json"));
+        ConstructItemDatabase();
+
+        Debug.Log(FetchItemByID(1).Description);
+    }
+
+    public Item FetchItemByID(int id)
+    {
+        for (int i = 0; i < database.Count; i++)
+        {
+            if (id == database[i].ID)
+            {
+                return database[i];
+            }
+        }
+        return null;
+    }
+
+
+    void ConstructItemDatabase()
+    {
+        for (int i = 0; i < itemData.Count; i++)
+        {
+            database.Add(new Item((int)itemData[i]["id"],
+                (string)itemData[i]["title"],
+                (int)itemData[i]["value"],
+                (int)itemData[i]["stats"]["attack"],
+                (int)itemData[i]["stats"]["defence"],
+                (int)itemData[i]["stats"]["power"],
+                (string)itemData[i]["description"],
+                (bool)itemData[i]["stackable"],
+                (string)itemData[i]["slug"]
+                ));
+        }
+    }
+}
+
+
+public class Item
+{
+    public int ID { get; set; }
+    public string Title { get; set; }
+    public int Value { get; set; }
+    public int Attack { get; set; }
+    public int Defence { get; set; }
+    public int Power { get; set; }
+    public string Description { get; set; }
+    public bool Stackable { get; set; }
+    public string Slug { get; set; }
+    public Sprite Sprite { get; set; }
+
+    public Item(int id, string title, int value, int attack, int defence, int power, string description, bool stackable, string slug)
+    {
+        this.ID = id;
+        this.Title = title;
+        this.Value = value;
+        this.Attack = attack;
+        this.Defence = defence;
+        this.Power = power;
+        this.Description = description;
+        this.Stackable = stackable;
+        this.Slug = slug;
+        this.Sprite = Resources.Load<Sprite>("Sprites/Items/" + slug);
+    }
+
+    public Item()
+    {
+        this.ID = -1;
+    }
+}
