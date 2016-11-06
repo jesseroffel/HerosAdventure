f92d75a28e1ddd28ad2f9819e1fb4ed9dcf558fc
quintin iwema <quintiniwema@hotmail.com>
2016-11-07 00:15:14 +0100

    dungeon tp / started demo dungeon

---
+++ Game/Assets/Scripts/Dungeon/portal.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class portal : MonoBehaviour
+{
+    public Transform entrance;
+    public Transform exit;
+    GameObject player;
+
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    void OnTriggerEnter(Collider col)
+    {
+            if(transform.position == entrance.transform.position)
+            {
+                player.transform.position = exit.transform.position;
+            }
+            else
+            {
+                player.transform.position = entrance.transform.position;
+             }
+    }
+}
