43d5b3fc33b4d6858127756558a691a8b8d8786e
Jesse Roffel <jesseroffel@live.nl>
2016-11-14 21:41:52 +0100

    WIP Enemy attacks. Added Effects and Hits

--- Game/Assets/Scripts/Enemy/EnemyHP.cs
+++ Game/Assets/Scripts/Enemy/EnemyHP.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 
 public class EnemyHP : MonoBehaviour {
-    
-    public Material HitMateral;
+
+    public Material[] Materials;
+    // 0 = own
+    // 1 = hit
+    // 2 = slowness
+    // 3 = poison
+    // 4 = fire
+    private int currentmat = 0;
     private Renderer rend;
-    //private Color HitColor = Color.red;
-    //public Material thisMaterial;
-    private Material OwnMaterial;
-    //public Color thisColor;
 
     public float currentHP;
     private int EnemyID = 1;
@@ -18,14 +20,18 @@ public class EnemyHP : MonoBehaviour {
     private bool isHit = false;
     public bool defeated = false;
     private float HitTime = 0;
-    private float HitCoolDown = 0;
+    private float HitCoolDown = 0.25f;
     private bool Sendkill = false;
     private bool HitKnockback = false;
     private Vector3 KnockbackForce;
 
     //Effects
     EnemyAI EAI;
-    float oldspeed = 0;
+
+    private float oldspeed = 0;
+    private float poisondamage = 0;
+    private float poisonwait = 0;
+    private bool poison = false;
 
     enum EffectNames { Test, MagicMissle, AeOSlowdown, AoEPoison}
 
@@ -33,17 +39,15 @@ public class EnemyHP : MonoBehaviour {
     void Start () {
         //SetEnemyID();
         rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = Materials[0];
         if (MaxHP <= 0) {
             if (EnemyID == 1)
             {
                 MaxHP = 100;
-                HitCoolDown = 1.0f;
             }
         }
         currentHP = MaxHP;
-        //thisMaterial = rend.material;
-        //OwnMaterial = thisMaterial;
-        //Debug.Log(thisMaterial.name);
 
         if (EAI == null)
         {
@@ -56,13 +60,16 @@ public class EnemyHP : MonoBehaviour {
         {
             shrinkobject();
         }
+        
+        CheckEffect();
+        CheckDebufs();
+
         if (isHit && Time.time > HitTime)
         {
+            rend.sharedMaterial = Materials[currentmat];
             isHit = false;
             //if (thisMaterial) { rend.material.CopyPropertiesFromMaterial(thisMaterial); }
         }
-        CheckEffect();
-
     }
 
     void FixedUpdate()
@@ -72,6 +79,19 @@ public class EnemyHP : MonoBehaviour {
             HitKnockback = false;
             transform.GetComponent<Rigidbody>().AddForce(KnockbackForce, ForceMode.Impulse);
         }
+    }
+
+    void CheckDebufs()
+    {
+        if (poison)
+        {
+            if (poisonwait < Time.time)
+            {
+                HitTarget(poisondamage, Vector3.zero);
+                poisonwait = Time.time + 1;
+            }
+        }
+
     }
 
     void SetEnemyID()
@@ -91,24 +111,24 @@ public class EnemyHP : MonoBehaviour {
         currentHP = currentHP - damage;
         if (currentHP > 0)
         {
-            Debug.Log("[ENEMY] " + gameObject.name + " damaged, HP:  " + currentHP + "  D:    " + damage + "  MHP:  " + MaxHP);
+            Debug.Log("[ENEMY] " + gameObject.name + " damaged. [HP] " + currentHP + " [DAMAGE] " + damage);
         }
         else
         {
             if (currentHP < 0) { currentHP = 0; }
-            Debug.Log("[ENEMY] " + gameObject.name + " defeated, HP:  " + currentHP + "  D:     " + damage + "  MHP:  " + MaxHP);
+            Debug.Log("[ENEMY] " + gameObject.name + " damaged. [HP] " + currentHP + " [DAMAGE] " + damage + " [MAXHP] " + MaxHP);
             defeated = true;
             // Notify Questlist of kill for quests
             QuestList.QuestListObject.RegisterKillID(EnemyID);
 
-            if (HitMateral) { rend.material.CopyPropertiesFromMaterial(HitMateral); }
+            rend.sharedMaterial = Materials[1];
         }
         isHit = true;
         HitTime = Time.time + HitCoolDown;
+        rend.sharedMaterial = Materials[1];
         //if (HitMateral) { rend.material.CopyPropertiesFromMaterial(HitMateral); }
         KnockbackForce = force;
         HitKnockback = true;
-        
     }
 
     void shrinkobject()
@@ -127,10 +147,10 @@ public class EnemyHP : MonoBehaviour {
     {
         EffectsApplied.Add(effectID);
         EffectTime.Add(Time.time + duration);
-        ApplyActiveEffects();
+        ApplyActiveEffects(duration);
     }
 
-    void ApplyActiveEffects()
+    void ApplyActiveEffects(float duration)
     {
         int Amount = EffectsApplied.Count;
         if (Amount > 0)
@@ -144,6 +164,7 @@ public class EnemyHP : MonoBehaviour {
                         DebufSpeed();
                         break;
                     case 3:
+                        DebufPoison();
                         break;
 
                 }
@@ -178,8 +199,33 @@ public class EnemyHP : MonoBehaviour {
 
     void DebufSpeed()
     {
+        currentmat = 2;
         if (oldspeed == 0) { oldspeed = EAI.speed; }
         EAI.SetSpeed(EAI.speed / 2);
+        rend.sharedMaterial = Materials[currentmat];
+        //rend.sharedMaterial.CopyPropertiesFromMaterial(Materials[0]);
+    }
+
+    void DebufBurn()
+    {
+        currentmat = 4;
+        rend.sharedMaterial = Materials[currentmat];
+    }
+
+    public void SetMaterial(int index)
+    {
+        rend.sharedMaterial = Materials[index];
+
+    }
+
+    void DebufPoison()
+    {
+        currentmat = 3;
+        if (poison == false) { poison = true; }
+        poisondamage += 10;
+        rend.sharedMaterial = Materials[currentmat];
+        poisonwait = Time.time + 1;
+        //rend.sharedMaterial.CopyPropertiesFromMaterial(Materials[0]);
     }
 
     bool RemoveEffect(int Idex)
@@ -201,7 +247,7 @@ public class EnemyHP : MonoBehaviour {
         {
             if (EffectsApplied[i] == Idex) { check = true; }
         }
-        if (!check) { RemoveDebuff(Idex); }
+        if (!check) { RemoveDebuff(Idex); rend.sharedMaterial = Materials[0]; }
         if (endcheck) { return true; } else { return false; }
     }
 
@@ -214,6 +260,7 @@ public class EnemyHP : MonoBehaviour {
                 oldspeed = 0;
                 break;
             case 3:
+                poison = false;
                 break;
         }
     }
